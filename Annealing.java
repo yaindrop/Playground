6f4844275d5bac5406ca931657a019fd9195367c
Yaindrop <yuan_dian@live.com>
2018-12-27 12:22:31 -0800

    Add files via upload

--- Annealing.java
+++ Annealing.java
@@ -1,30 +1,28 @@
+package Playground;
+
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.StandardOpenOption;
 import java.util.*;
 import java.util.function.*;
 
+/*
+ * Simple mathematical function graph drawer using annealing algorithm
+ */
+
 public class Annealing {
-    static class Point implements Comparable<Point>{
+    interface QuadFunction<A, B, C, D, R> {R apply(A a, B b, C c, D d);}
+    static class Point {
         double x, y;
         Point(double x, double y) {
             this.x = x;
             this.y = y;
         }
-        @Override
-        public String toString() {
-            return "(" + x +"," + y +")";
-        }
-        @Override
-        public int compareTo(Point o) {
-            return x > o.x ? 1 : -1;
-        }
     }
     public static void main(String[] args) {
-        int size = 200;
-        double lower = -10, upper = 10, step = (upper -lower) / size;
-        ArrayList<Point> list = new ArrayList<>(size * size);
-        for (double x = lower; x < upper; x += step) for (double y = lower; y < upper; y += step) list.add(new Point(x, y));
+        int size = 100;
+        double xLower = -10, xUpper = 10, xStep = (xUpper -xLower) / size;
+        double yLower = -10, yUpper = 10, yStep = (yUpper -yLower) / size;
 
         final BiFunction<Double, Double, Double> f = (x, y) ->
                 Math.sin(x) - y;
@@ -36,43 +34,49 @@ public class Annealing {
         final Function<Point, Double> potential = p -> Math.abs(f.apply(p.x, p.y));
         double Kb = 1.38064852E-23;
         final BiFunction<Double, Double, Double> metropolis = (delE, T) -> Math.exp(-delE/(T*Kb));
+        final BiFunction<Double, Double, Double> random = (l, u) -> l + Math.random() * (u - l);
+        final QuadFunction<Double, Double, Double, Double, Double> restrictedRandom = (x, r, xu, xl) ->
+            random.apply(Math.max(xl, x - r), Math.min(xu, x + r));
 
-        double T = 5000;
-        while (T > 0) {
-            for (Point p : list) {
-                Point rand = new Point(lower + Math.random() * (upper - lower), lower + Math.random() * (upper - lower));
-                double delE = potential.apply(rand) - potential.apply(p);
+        List<Point> points = new ArrayList<>(size * size);
+        for (double x = xLower; x < xUpper; x += xStep) for (double y = yLower; y < yUpper; y += yStep) points.add(new Point(x, y));
+
+        double startT = 5000, endT = 1, decayRate = 0.999;
+        for (double T = startT; T > endT; T *= decayRate) {
+            for (Point p : points) {
+                Point next = new Point(
+                        restrictedRandom.apply(p.x, (xUpper - xLower) * Math.log(T) / Math.log(startT), xUpper, xLower),
+                        restrictedRandom.apply(p.y, (yUpper - yLower) * Math.log(T) / Math.log(startT), yUpper, yLower)
+                );
+                double delE = potential.apply(next) - potential.apply(p);
                 if (Math.random() < metropolis.apply(delE, T)) {
-                    p.x = rand.x;
-                    p.y = rand.y;
+                    p.x = next.x;
+                    p.y = next.y;
                 }
             }
-            T -= 10;
             System.out.println(T);
         }
-
-        Collections.sort(list);
 
         StringBuilder xStr = new StringBuilder();
         StringBuilder yStr = new StringBuilder();
         xStr.append("x = [");
         yStr.append("y = [");
-        for (int i = 0; i < list.size(); i ++) {
-            Point p = list.get(i);
+        for (int i = 0; i < points.size(); i ++) {
+            Point p = points.get(i);
             xStr.append(p.x);
             yStr.append(p.y);
-            if (i != list.size() - 1) {
+            if (i != points.size() - 1) {
                 xStr.append(',');
                 yStr.append(',');
             }
         }
         xStr.append(']');
         yStr.append(']');
+
         List<String> strs = new ArrayList<>();
         strs.add(xStr.toString());
         strs.add(yStr.toString());
         Path out = Paths.get("out.txt");
-
         try {
             Files.write(out, strs, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
         } catch (IOException e) {
